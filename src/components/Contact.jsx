1119a1cbbd1100ab2bb4e9029e3d0bc72324032b
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2026-01-04 22:47:39 +0100

    remove unused code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { FaInstagramSquare, FaFacebookSquare } from "react-icons/fa";
 import { FaSquarePhone } from "react-icons/fa6"
 import { MdEmail } from "react-icons/md";
